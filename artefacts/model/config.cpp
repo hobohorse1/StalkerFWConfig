ee16ba5fcbc4e7d8a6f2a7341002dc25db2b51db
hobohorse1 <46578033+hobohorse1@users.noreply.github.com>
2025-11-08 18:21:51 +0000

    Format artefact classes consistently

--- artefacts/model/config.cpp
+++ artefacts/model/config.cpp
@@ -245,22 +245,255 @@ class CfgVehicles
 		hiddenSelectionsMaterials[] = {""};
 	};
 	
-	class SFW_Item_Artefact_Lens: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/Lens/Lens.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_Light: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/Light/Light.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_MamasBeads: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/MamasBeads/MamasBeads.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_MeatChunk: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/MeatChunk/MeatChunk.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_Monolith: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/Monolith/Monolith.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_NanoCell: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/NanoCell/NanoCell.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_NightStar: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/NightStar/NightStar.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_Oasis: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/Oasis/Oasis.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_SeaUrchin: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/SeaUrchin/SeaUrchin.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_ShineOfTheForest: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/ShineOfTheForest/ShineOfTheForest.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_SnowFlake: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/SnowFlake/SnowFlake.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_Soul: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/Soul/Soul.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_SparklerSpring: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/SparklerSpring/SparklerSpring.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_SteeringWheel: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/SteeringWheel/SteeringWheel.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_StoneBlood: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/StoneBlood/StoneBlood.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_StoneFlower: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/StoneFlower/StoneFlower.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_Thorn: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/Thorn/Thorn.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
-	class SFW_Item_Artefact_Twist: SFW_Item_Artefact_Base { scope=2; displayName=""; descriptionShort=""; model="StalkerFWConfig/artefacts/model/Twist/Twist.p3d"; rotationFlags=0; weight=200; itemSize[]={2,2}; hiddenSelections[] = {"zbytek"}; hiddenSelectionsTextures[] = {""}; hiddenSelectionsMaterials[] = {""}; };
+        class SFW_Item_Artefact_Lens: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/Lens/Lens.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_Light: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/Light/Light.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_MamasBeads: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/MamasBeads/MamasBeads.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_MeatChunk: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/MeatChunk/MeatChunk.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_Monolith: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/Monolith/Monolith.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_NanoCell: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/NanoCell/NanoCell.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_NightStar: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/NightStar/NightStar.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_Oasis: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/Oasis/Oasis.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_SeaUrchin: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/SeaUrchin/SeaUrchin.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_ShineOfTheForest: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/ShineOfTheForest/ShineOfTheForest.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_SnowFlake: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/SnowFlake/SnowFlake.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_Soul: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/Soul/Soul.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_SparklerSpring: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/SparklerSpring/SparklerSpring.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_SteeringWheel: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/SteeringWheel/SteeringWheel.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_StoneBlood: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/StoneBlood/StoneBlood.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_StoneFlower: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/StoneFlower/StoneFlower.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_Thorn: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/Thorn/Thorn.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
+
+        class SFW_Item_Artefact_Twist: SFW_Item_Artefact_Base
+        {
+                scope=2;
+                displayName="";
+                descriptionShort="";
+                model="StalkerFWConfig/artefacts/model/Twist/Twist.p3d";
+                rotationFlags=0;
+                weight=200;
+                itemSize[]={2,2};
+                hiddenSelections[] = {"zbytek"};
+                hiddenSelectionsTextures[] = {""};
+                hiddenSelectionsMaterials[] = {""};
+        };
 };
